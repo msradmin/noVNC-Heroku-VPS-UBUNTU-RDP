89f5080daee1227659a5e38fbbda73c56e809a67
msradmin <86022985+msradmin@users.noreply.github.com>
2022-06-15 16:19:47 -0700

    Update iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -3,4 +3,4 @@ qemu-img create -f qcow2 hd.img 90G
 echo Executando o download da iso para o sistema
 wget www.ittics.com/?dl_id=85
 echo executando o sistema
-qemu-system-x86_64 -m 2048 -cdrom Win10_2004_x64.iechso -hda hd.img -netdev user,id=n0 -device rtl8139,netdev=n0 -vnc 127.0.0.1:2
+qemu-system-x86_64 -m 2048 -cdrom Win10_2004_x64.iso -hda hd.img -netdev user,id=n0 -device rtl8139,netdev=n0 -vnc 127.0.0.1:2
