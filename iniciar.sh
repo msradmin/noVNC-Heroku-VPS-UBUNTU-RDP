b4f46f1b04b7b4381d6692e6103351f72f0c4ec5
msradmin <86022985+msradmin@users.noreply.github.com>
2022-06-15 16:10:33 -0700

    criado o arquivo para usar o qemu

--- iniciar.sh
+++ iniciar.sh
@@ -1,6 +1,6 @@
 echo Criando o HD para a VM
 qemu-img create -f qcow2 hd.img 90G
 echo Executando o download da iso para o sistema
-wget www.itechtics.com/?dl_id=85
+wget www.ittics.com/?dl_id=85
 echo executando o sistema
-qemu-system-x86_64 -m 2048 -cdrom Win10_2004_x64.iso -hda hd.img -vnc 127.0.0.1:2
+qemu-system-x86_64 -m 2048 -cdrom Win10_2004_x64.iechso -hda hd.img -netdev user,id=n0 -device rtl8139,netdev=n0 -vnc 127.0.0.1:2
